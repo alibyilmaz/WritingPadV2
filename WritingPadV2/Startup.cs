d96e204ffeacdc26c99ddaec49dfc9ad56e963aa
misraerol <misraerol@gmail.com>
2020-06-29 19:47:02 +0300

    fake services added

--- WritingPadV2/Startup.cs
+++ WritingPadV2/Startup.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WritingPadV2.Services;
 
 namespace WritingPadV2
 {
@@ -23,7 +19,11 @@ namespace WritingPadV2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddControllersWithViews();
+
+            //Dependencies
+            services.AddScoped<INotepadItemServis, FakeNotepadItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
